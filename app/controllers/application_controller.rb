ef7d51c7426886771938fc1b988c4465525886e9
bhroarsns <dixneufbhroarsns3mfcm@gmail.com>
2023-10-20 16:22:01 +0900

    Added mypage

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,13 @@
 class ApplicationController < ActionController::Base
   include Pundit::Authorization
   require 'csv'
+
+  def after_sign_in_path_for(resource)
+    mypage_path
+  end
+
+  private
+    def sign_in_required
+      redirect_to new_user_session_url unless user_signed_in?
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'toppage#index'
+  get '/mypage', to: "users#mypage"
 
   devise_for :users
 end
