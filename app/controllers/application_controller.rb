a2e93dfd8f23722fefcc41ca8dcf0bfada7208c4
bhroarsns <dixneufbhroarsns3mfcm@gmail.com>
2023-10-20 16:32:20 +0900

    Bug on user name removed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,9 +5,20 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     mypage_path
   end
+  
+  protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
     def sign_in_required
       redirect_to new_user_session_url unless user_signed_in?
+    end
+
+  protected
+    def configure_permitted_parameters
+      added_attrs = [ :email, :name, :password, :password_confirmation ]
+      devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+      devise_parameter_sanitizer.permit :account_update, keys: added_attrs
+      devise_parameter_sanitizer.permit :sign_in, keys: added_attrs
     end
 end
